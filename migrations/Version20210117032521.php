5c482cca6acd9e093c0ddf84d54c7d431a227c3a
tienvx <tien.xuan.vo@gmail.com>
2021-01-29 23:28:06 +0700

    Update migration

--- migrations/Version20210117032521.php
+++ migrations/Version20210117032521.php
@@ -27,6 +27,7 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('CREATE SEQUENCE mail_log_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE mail_template_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE model_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE revision_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE task_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE user_group_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE user_profile_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
@@ -35,8 +36,8 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('CREATE TABLE app_config (id INT NOT NULL, name VARCHAR(190) NOT NULL, value TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_318942FC5E237E06 ON app_config (name)');
         $this->addSql('COMMENT ON COLUMN app_config.value IS \'(DC2Type:simple_array)\'');
-        $this->addSql('CREATE TABLE bug (id INT NOT NULL, task_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, steps TEXT NOT NULL, message TEXT NOT NULL, closed BOOLEAN NOT NULL, model_version INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, progress_total INT DEFAULT 0 NOT NULL, progress_processed INT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_358CBF148DB60186 ON bug (task_id)');
+        $this->addSql('CREATE TABLE bug (id INT NOT NULL, task_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, steps TEXT NOT NULL, message TEXT NOT NULL, closed BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, progress_total INT DEFAULT 0 NOT NULL, progress_processed INT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_358CBF148DB60186 ON bug (task_id)');
         $this->addSql('COMMENT ON COLUMN bug.steps IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE mail_log (id INT NOT NULL, mTo TEXT NOT NULL, mFrom TEXT NOT NULL, subject VARCHAR(255) DEFAULT NULL, body TEXT DEFAULT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, template_id VARCHAR(75) DEFAULT NULL, language VARCHAR(3) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN mail_log.mTo IS \'(DC2Type:array)\'');
@@ -44,11 +45,15 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN mail_log.body IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE mail_template (id INT NOT NULL, template_id VARCHAR(50) NOT NULL, template TEXT DEFAULT NULL, template_data TEXT DEFAULT NULL, subject VARCHAR(255) DEFAULT NULL, language VARCHAR(3) NOT NULL, status BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN mail_template.template_data IS \'(DC2Type:array)\'');
-        $this->addSql('CREATE TABLE model (id INT NOT NULL, author INT DEFAULT NULL, label VARCHAR(255) NOT NULL, tags VARCHAR(255) DEFAULT NULL, places TEXT NOT NULL, transitions TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, version INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN model.places IS \'(DC2Type:array)\'');
-        $this->addSql('COMMENT ON COLUMN model.transitions IS \'(DC2Type:array)\'');
-        $this->addSql('CREATE TABLE task (id INT NOT NULL, model_id INT NOT NULL, title VARCHAR(255) NOT NULL, author INT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, selenium_config_provider VARCHAR(255) NOT NULL, selenium_config_platform VARCHAR(255) NOT NULL, selenium_config_browser VARCHAR(255) NOT NULL, selenium_config_browser_version VARCHAR(255) NOT NULL, selenium_config_resolution VARCHAR(255) NOT NULL, task_config_generator VARCHAR(255) NOT NULL, task_config_generator_config TEXT NOT NULL, task_config_reducer VARCHAR(255) NOT NULL, task_config_notify_author BOOLEAN NOT NULL, task_config_notify_channels TEXT NOT NULL, progress_total INT DEFAULT 0 NOT NULL, progress_processed INT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_527EDB257975B7E7 ON task (model_id)');
+        $this->addSql('CREATE TABLE model (id INT NOT NULL, active_revision_id INT DEFAULT NULL, revisions_id INT DEFAULT NULL, author INT DEFAULT NULL, label VARCHAR(255) NOT NULL, tags VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79572D9490589C4 ON model (active_revision_id)');
+        $this->addSql('CREATE INDEX IDX_D79572D99A6BD9E8 ON model (revisions_id)');
+        $this->addSql('CREATE TABLE revision (id INT NOT NULL, model_id INT DEFAULT NULL, places TEXT NOT NULL, transitions TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_6D6315CC7975B7E7 ON revision (model_id)');
+        $this->addSql('COMMENT ON COLUMN revision.places IS \'(DC2Type:array)\'');
+        $this->addSql('COMMENT ON COLUMN revision.transitions IS \'(DC2Type:array)\'');
+        $this->addSql('CREATE TABLE task (id INT NOT NULL, model_revision_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, author INT DEFAULT NULL, running BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, selenium_config_provider VARCHAR(255) NOT NULL, selenium_config_platform VARCHAR(255) NOT NULL, selenium_config_browser VARCHAR(255) NOT NULL, selenium_config_browser_version VARCHAR(255) NOT NULL, selenium_config_resolution VARCHAR(255) NOT NULL, task_config_generator VARCHAR(255) NOT NULL, task_config_generator_config TEXT NOT NULL, task_config_reducer VARCHAR(255) NOT NULL, task_config_notify_author BOOLEAN NOT NULL, task_config_notify_channels TEXT NOT NULL, progress_total INT DEFAULT 0 NOT NULL, progress_processed INT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_527EDB2573F84563 ON task (model_revision_id)');
         $this->addSql('COMMENT ON COLUMN task.task_config_generator_config IS \'(DC2Type:array)\'');
         $this->addSql('COMMENT ON COLUMN task.task_config_notify_channels IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE user_group (id INT NOT NULL, name VARCHAR(180) NOT NULL, roles TEXT NOT NULL, PRIMARY KEY(id))');
@@ -82,7 +87,10 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
         $this->addSql('COMMIT;');
         $this->addSql('ALTER TABLE bug ADD CONSTRAINT FK_358CBF148DB60186 FOREIGN KEY (task_id) REFERENCES task (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB257975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE model ADD CONSTRAINT FK_D79572D9490589C4 FOREIGN KEY (active_revision_id) REFERENCES revision (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE model ADD CONSTRAINT FK_D79572D99A6BD9E8 FOREIGN KEY (revisions_id) REFERENCES revision (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE revision ADD CONSTRAINT FK_6D6315CC7975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB2573F84563 FOREIGN KEY (model_revision_id) REFERENCES revision (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E9CCFA12B8 FOREIGN KEY (profile_id) REFERENCES user_profile (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE user_group_tax ADD CONSTRAINT FK_DF3DFEBA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE user_group_tax ADD CONSTRAINT FK_DF3DFEBFE54D947 FOREIGN KEY (group_id) REFERENCES user_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -95,7 +103,10 @@ final class Version20210117032521 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB257975B7E7');
+        $this->addSql('ALTER TABLE revision DROP CONSTRAINT FK_6D6315CC7975B7E7');
+        $this->addSql('ALTER TABLE model DROP CONSTRAINT FK_D79572D9490589C4');
+        $this->addSql('ALTER TABLE model DROP CONSTRAINT FK_D79572D99A6BD9E8');
+        $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB2573F84563');
         $this->addSql('ALTER TABLE bug DROP CONSTRAINT FK_358CBF148DB60186');
         $this->addSql('ALTER TABLE user_group_tax DROP CONSTRAINT FK_DF3DFEBFE54D947');
         $this->addSql('ALTER TABLE users DROP CONSTRAINT FK_1483A5E9CCFA12B8');
@@ -106,6 +117,7 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('DROP SEQUENCE mail_log_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE mail_template_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE model_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE revision_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE task_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE user_group_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE user_profile_id_seq CASCADE');
@@ -116,6 +128,7 @@ final class Version20210117032521 extends AbstractMigration
         $this->addSql('DROP TABLE mail_log');
         $this->addSql('DROP TABLE mail_template');
         $this->addSql('DROP TABLE model');
+        $this->addSql('DROP TABLE revision');
         $this->addSql('DROP TABLE task');
         $this->addSql('DROP TABLE user_group');
         $this->addSql('DROP TABLE user_profile');
