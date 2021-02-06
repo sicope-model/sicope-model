fcc2f4214b543eb861a8abfbd8382517659ab223
tien.xuan.vo <tien.xuan.vo@gmail.com>
2021-02-06 12:37:25 +0700

    Add Slack channel

---
+++ src/Channel/SlackChannel.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the SICOPE Model package.
+ *
+ * @package     sicope-model
+ * @license     LICENSE
+ * @author      Ramazan APAYDIN <apaydin541@gmail.com>
+ * @author      Tien Xuan Vo <tien.xuan.vo@gmail.com>
+ * @link        https://github.com/sicope-model/sicope-model
+ */
+
+namespace App\Channel;
+
+use Symfony\Component\Notifier\Bridge\Slack\SlackTransport;
+use Tienvx\Bundle\MbtBundle\Channel\AbstractChannel;
+
+class SlackChannel extends AbstractChannel
+{
+    public static function getName(): string
+    {
+        return 'chat/slack';
+    }
+
+    public static function isSupported(): bool
+    {
+        return class_exists(SlackTransport::class);
+    }
+}
