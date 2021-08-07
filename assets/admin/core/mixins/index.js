d6d87d59c7fc4666d88c12d8b9b7e9a9715bb265
tienvx <tien.xuan.vo@gmail.com>
2021-08-07 11:50:54 +0700

    Create new project and install easyadmin

--- assets/admin/core/mixins/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-    install(app, options) {
-        app.mixin(require('./router').default)
-        app.mixin(require('./dateFormatter').default)
-    }
-}
---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sicope Model');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
