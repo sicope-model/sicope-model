8c325bbbf26248f5bcf31d240252cf2f63fac26d
tienvx <tien.xuan.vo@gmail.com>
2021-08-16 08:21:32 +0700

    Install chartjs

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Tienvx\Bundle\MbtBundle\TienvxMbtBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
