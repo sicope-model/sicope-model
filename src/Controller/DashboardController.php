7d03c69e817afaeb93c9bb620d1e1812ad461762
tienvx <tien.xuan.vo@gmail.com>
2021-08-16 08:36:45 +0700

    Render chart in dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,13 +13,37 @@ use Symfony\Component\Routing\Annotation\Route;
 use Tienvx\Bundle\MbtBundle\Entity\Bug;
 use Tienvx\Bundle\MbtBundle\Entity\Model;
 use Tienvx\Bundle\MbtBundle\Entity\Task;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private ChartBuilderInterface $chartBuilder)
+    {
+
+    }
+
     #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
-        return parent::index();
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('dashboard.html.twig', [
+            'chart' => $chart,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
