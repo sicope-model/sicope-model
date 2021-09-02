38ce8bd36af65a380f25e1e483294660e7079c65
tienvx <tien.xuan.vo@gmail.com>
2021-09-02 23:30:01 +0700

    Show tasks, bugs and models charts

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Config;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -18,31 +19,38 @@ use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
-    public function __construct(private ChartBuilderInterface $chartBuilder)
-    {
+    private const TIME = '-12 Day';
+    private const COLUMNS = 12;
+    private const OPTIONS = [
+        'animation' => [
+            'duration' => 2000,
+            'easing' => 'easeOutQuart',
+        ],
+        'plugins' => [
+            'legend' => [
+                'display' => false,
+                'position' => 'top',
+            ],
+            'title' => [
+                'display' => false,
+                'position' => 'left',
+            ],
+        ],
+    ];
+
+    public function __construct(
+        private ChartBuilderInterface $chartBuilder,
+        private EntityManagerInterface $entityManager
+    ) {
 
     }
 
     #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
-        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            'datasets' => [
-                [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
-                ],
-            ],
-        ]);
-
-        $chart->setOptions([/* ... */]);
-
         return $this->render('dashboard.html.twig', [
-            'chart' => $chart,
+            'taskBugChart' => $this->getTaskBugChart(),
+            'modelChart' => $this->getModelChart(),
         ]);
     }
 
@@ -64,5 +72,105 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Config', 'fa fa-cogs', Config::class)
             ->setPermission('ROLE_ADMIN')
             ->setAction(Action::EDIT);
+    }
+
+    private function getTaskBugChart(): Chart
+    {
+        $date = new \DateTime(static::TIME);
+
+        // Load Records
+        $taskData = $this->entityManager->getRepository(Task::class)
+            ->createQueryBuilder('t')
+            ->select('count(t.id) as count, DAY(t.createdAt) as day')
+            ->groupBy('day')
+            ->where('t.createdAt >= :date')
+            ->setParameter('date', $date)
+            ->getQuery()->getArrayResult();
+        $bugData = $this->entityManager->getRepository(Bug::class)
+            ->createQueryBuilder('b')
+            ->select('count(b.id) as count, DAY(b.createdAt) as day')
+            ->groupBy('day')
+            ->where('b.createdAt >= :date')
+            ->setParameter('date', $date)
+            ->getQuery()->getArrayResult();
+        $taskData = array_column($taskData, 'count', 'day');
+        $bugData = array_column($bugData, 'count', 'day');
+
+        // Optimize Data
+        $labels = $tasks = $bugs = [];
+        for ($i = 0; $i < static::COLUMNS; ++$i) {
+            $day = explode('/', date('j/m', strtotime("-{$i} day")));
+            $labels[] = $day[0] . '/' . $day[1];
+            $tasks[] = $taskData[$day[0]] ?? 0;
+            $bugs[] = $bugData[$day[0]] ?? 0;
+        }
+
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Tasks',
+                    'backgroundColor' => 'rgb(255, 99, 132, 0.2)',
+                    'borderColor' => 'rgb(255, 99, 132, 0.8)',
+                    'fill' => true,
+                    'borderWidth' => 1,
+                    'data' => $tasks,
+                ],
+                [
+                    'label' => 'Bugs',
+                    'backgroundColor' => 'rgba(48, 164, 255, 0.2)',
+                    'borderColor' => 'rgba(48, 164, 255, 0.8)',
+                    'fill' => true,
+                    'borderWidth' => 1,
+                    'data' => $bugs,
+                ],
+            ],
+        ]);
+
+        $chart->setOptions(static::OPTIONS);
+
+        return $chart;
+    }
+
+    private function getModelChart(): Chart
+    {
+        $date = new \DateTime(static::TIME);
+
+        // Load Records
+        $modelData = $this->entityManager->getRepository(Task::class)
+            ->createQueryBuilder('t')
+            ->select('count(t.id) as count, DAY(t.createdAt) as day')
+            ->groupBy('day')
+            ->where('t.createdAt >= :date')
+            ->setParameter('date', $date)
+            ->getQuery()->getArrayResult();
+        $modelData = array_column($modelData, 'count', 'day');
+
+        // Optimize Data
+        $labels = $models = [];
+        for ($i = 0; $i < static::COLUMNS; ++$i) {
+            $day = explode('/', date('j/m', strtotime("-{$i} day")));
+            $labels[] = $day[0] . '/' . $day[1];
+            $models[] = $modelData[$day[0]] ?? 0;
+        }
+
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Models',
+                    'backgroundColor' => 'rgba(76, 175, 80, 0.5)',
+                    'borderColor' => '#6da252',
+                    'borderWidth' => 1,
+                    'data' => $models,
+                ],
+            ],
+        ]);
+
+        $chart->setOptions(static::OPTIONS);
+
+        return $chart;
     }
 }
