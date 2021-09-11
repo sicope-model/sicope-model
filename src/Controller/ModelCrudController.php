0326135a30902b7aa887a646e0af2eadeeacd0b5
tienvx <tien.xuan.vo@gmail.com>
2021-09-11 21:06:52 +0700

    Export model

--- src/Controller/ModelCrudController.php
+++ src/Controller/ModelCrudController.php
@@ -12,10 +12,17 @@
 namespace App\Controller;
 
 use App\Form\Model\RevisionType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\HiddenField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Tienvx\Bundle\MbtBundle\Entity\Model;
 use Tienvx\Bundle\MbtBundle\Model\Model\RevisionInterface;
 
@@ -52,5 +59,30 @@ class ModelCrudController extends AbstractCrudController
             ->addCssClass('field-collection')
             ->addJsFiles('bundles/easyadmin/form-type-collection.js')
             ->setDefaultColumns('col-md-8 col-xxl-7');
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $exportModel = Action::new('exportModel', 'Export Model', 'fa fa-file-export')
+            ->linkToCrudAction('exportModel');
+        $importModel = Action::new('importModel', 'Import Model', 'fa fa-file-import')
+            ->linkToRoute('app_import_model')
+            ->createAsGlobalAction();
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $exportModel)
+            ->add(Crud::PAGE_INDEX, $importModel);
+    }
+
+    public function exportModel(AdminContext $context): JsonResponse
+    {
+        $model = $context->getEntity()->getInstance();;
+
+        return $this->json($model->toArray(), 200, [
+            'Content-Disposition' => HeaderUtils::makeDisposition(
+                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                $model->getLabel() . '.json'
+            ),
+        ])->setEncodingOptions(JsonResponse::DEFAULT_ENCODING_OPTIONS | \JSON_PRETTY_PRINT);
     }
 }
