96b0a768518aedebed6263362388dc51310136e6
tienvx <tien.xuan.vo@gmail.com>
2021-10-31 12:34:36 +0700

    Show sessions on dashboard

--- src/Service/SessionHelper.php
+++ src/Service/SessionHelper.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the SICOPE Model package.
+ *
+ * @package     sicope-model
+ * @license     LICENSE
+ * @author      Tien Xuan Vo <tien.xuan.vo@gmail.com>
+ * @link        https://github.com/sicope-model/sicope-model
+ */
+
 namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -24,6 +33,31 @@ class SessionHelper
         }
 
         return $choices;
+    }
+
+    public function getSessionStatistics(): array
+    {
+        $status = $this->getStatus();
+        $browsers = $status['browsers'] ?? [];
+        $sessions = [];
+        foreach ($browsers as $browser => $versions) {
+            foreach ($versions as $version => $users) {
+                $sessions[] = [
+                    'id' => "$browser:$version",
+                    'browser' => $this->formatter->format($browser, $version),
+                    'icon' => 'chrome-mobile' === $browser ? 'fa fa-mobile-alt' : "fab fa-$browser",
+                    'count' => array_reduce($users, fn (int $total, array $user) => $total + $user['count'], 0),
+                ];
+            }
+        }
+
+        return [
+            'total' => $status['total'],
+            'used' => $status['used'],
+            'queued' => $status['queued'],
+            'pending' => $status['pending'],
+            'sessions' => $sessions,
+        ];
     }
 
     protected function getStatus(): array
