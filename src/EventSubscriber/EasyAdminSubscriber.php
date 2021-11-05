a58e80563d4468206e4677730e2fad52ee9d31d2
tienvx <tien.xuan.vo@gmail.com>
2021-11-05 21:44:59 +0700

    Delete log and video on deleting bug and task

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -12,25 +12,27 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityDeletedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Tienvx\Bundle\MbtBundle\Entity\Bug;
 use Tienvx\Bundle\MbtBundle\Entity\Model;
 use Tienvx\Bundle\MbtBundle\Entity\Task;
+use Tienvx\Bundle\MbtBundle\Service\SelenoidHelperInterface;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
-    private Security $security;
-
-    public function __construct(Security $security)
+    public function __construct(private Security $security, private HttpClientInterface $client, private SelenoidHelperInterface $selenoidHelper)
     {
-        $this->security = $security;
     }
 
     public static function getSubscribedEvents()
     {
         return [
             BeforeEntityPersistedEvent::class => ['setAuthor'],
+            AfterEntityDeletedEvent::class => ['deleteVideoAndLog'],
         ];
     }
 
@@ -46,5 +48,17 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         if ($user instanceof User) {
             $entity->setAuthor($user->getId());
         }
+    }
+
+    public function deleteVideoAndLog(AfterEntityDeletedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!($entity instanceof Task) && !($entity instanceof Bug)) {
+            return;
+        }
+
+        $this->client->request('DELETE', $this->selenoidHelper->getVideoUrl($entity));
+        $this->client->request('DELETE', $this->selenoidHelper->getLogUrl($entity));
     }
 }
