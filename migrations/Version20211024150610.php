a036f637ea0ab707d59ea738a9277e4580ae08cf
tienvx <tien.xuan.vo@gmail.com>
2022-04-10 21:04:12 +0700

    Remove old migration

--- migrations/Version20211024150610.php
+++ migrations/Version20211024150610.php
@@ -29,18 +29,12 @@ final class Version20211024150610 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE app_config_id_seq CASCADE');
         $this->addSql('CREATE TABLE craue_config_setting (name VARCHAR(255) NOT NULL, section VARCHAR(255) DEFAULT NULL, value VARCHAR(255) DEFAULT NULL, PRIMARY KEY(name))');
-        $this->addSql('DROP TABLE app_config');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE app_config_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE app_config (id INT NOT NULL, name VARCHAR(190) NOT NULL, value TEXT DEFAULT NULL, type VARCHAR(150) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_318942fc5e237e06 ON app_config (name)');
         $this->addSql('DROP TABLE craue_config_setting');
     }
 }
